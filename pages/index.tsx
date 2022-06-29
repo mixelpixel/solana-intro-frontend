ac0ab26e45f5be2dcb00ae1a3d35725c87d681ff
Patrick Kennedy <mixelpix@mac.com>
2022-06-29 15:03:33 -0600

    demo3: set address balance

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as Web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
@@ -11,7 +12,13 @@ const Home: NextPage = () => {
 
   const addressSubmittedHandler = (address: string) => {
     setAddress(address)
-    setBalance(1000)
+    const key = new Web3.PublicKey(address)
+    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+    connection.getBalance(key)
+    .then(balance => {
+        setBalance(balance)
+    })
+    .catch(err => { throw err })
   }
 
   return (
