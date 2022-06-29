61c2d8982c4fa99d455d654abaa836dd75071e2f
Patrick Kennedy <mixelpix@mac.com>
2022-06-29 17:08:37 -0600

    Challenge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
+// import Head from 'next/head'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
 import * as Web3 from '@solana/web3.js'
@@ -9,6 +9,7 @@ import * as Web3 from '@solana/web3.js'
 const Home: NextPage = () => {
   const [balance, setBalance] = useState<number|string>(0) // <-- Typescripting!
   const [address, setAddress] = useState('')
+  const [exec, setExec] = useState<boolean|string>('🤔')
 
   const addressSubmittedHandler = (address: string) => {
     try {
@@ -19,9 +20,21 @@ const Home: NextPage = () => {
         .then(bal => {
             setBalance(bal / Web3.LAMPORTS_PER_SOL)
         })
+        connection.getAccountInfo(key)
+        .then(accInfo => {
+            if(accInfo?.executable) { // optional chaining
+                setExec('yep')
+            } else if (accInfo?.executable === undefined) {
+                setExec('is this an untouched account?')
+            } else {
+                setExec('nope')
+            }
+        })
     } catch (err) {
         setAddress(`${err}`) // <-- 🤓
         setBalance('Invalid') // <-- cuz Typescripting!
+        setExec('😅')
+
         alert(err)
     }
   }
@@ -35,6 +48,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is executable? ${exec}`}</p>
       </header>
     </div>
   )
